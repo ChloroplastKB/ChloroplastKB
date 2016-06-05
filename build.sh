887638766a8eeb2a0415ee5bfb8a417fad45b1eb
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-06-05 17:51:15 +0530

    Initial commit
    build.sh, shell script to --/build/-- whole "ChloroplastKB®" in one go.
    Usage: $ sh build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+
+# ========================== ChloroplastKB® ===========================
+#   A Capsid proteins and their associated functional genes database.
+#                                 CBBL®
+#           Computational Biology and Bioinformatics Laboratory
+#         Υπολογιστικής Βιολογίας και Βιοπληροφορικής Εργαστήριο
+#         Handcrafted by Prabhat Kumar, http://prabhatkumar.org/.
+# Copyright © 2014 - 2016, Sequømics Corporation. All rights reserved.
+# =====================================================================
