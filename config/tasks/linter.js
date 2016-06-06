6f3312c9bad6be2c27627c16668010ba152e61cf
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-06-06 18:22:41 +0530

    Initial commit
    Path: config/tasks/linter.js
    Usage: $ npm install grunt-contrib-jshint --save-dev

---
+++ config/tasks/linter.js
@@ -0,0 +1,34 @@
+/*!
+ * ChloroplastKB® : A Chloroplast proteins and their associated functional genes database.
+ * @version       : [1.0.0]
+ * _______________________________________________________________________________________
+ * CBBL®
+ * ~~~~~
+ * Computational Biology and Bioinformatics Laboratory
+ * Υπολογιστικής Βιολογίας και Βιοπληροφορικής Εργαστήριο
+ * _______________________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * _______________________________________________________________________________________
+ * @date      : 06-June-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * @require   : grunt-contrib-jshint
+ * _______________________________________________________________________________________
+ *      Licensed under the Apache License, Version 2.0, (the "License");
+ *      you may not use this file except in compliance with the License.
+ *                  You may obtain a copy of the License at:
+ *                 http://www.apache.org/licenses/LICENSE-2.0
+ *                                     or
+ *        https://github.com/ChloroplastKB/ChloroplastKB/blob/master/LICENSE
+ *    Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS-IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *                        limitations under the License.
+ * _______________________________________________________________________________________
+ */
